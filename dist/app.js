06ad22ad58e327256cfa71281b8823d3b41ff197
Muchnik Illia <muchnik.illia.js@gmail.com>
2021-10-29 10:46:03 +0300

    Add getRetes without testing

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,26 +19,31 @@ class CryptoPay {
         this.api_secret = api_secret;
         this.api_key = api_key;
         this.callback_secret = callback_secret;
+        //Rates
+        this.getRetes = () => __awaiter(this, void 0, void 0, function* () {
+            const path = '/api/rates';
+            const headers = this.headerCreator("GET", path);
+            // const response = await axios.get(path, headers)
+            // console.log({response})
+            // return response
+        });
         this.headerCreator = (method, path, body) => {
             const date = new Date().toUTCString();
             const contentType = "application/json";
             const bodyHash = body ? md5_1.default(body).toString() : "";
             const stringToSign = `${method}\n${bodyHash}\n${contentType}\n${date}\n${path}`;
             const signature = enc_base64_1.default.stringify(sha1_1.default(stringToSign, this.api_secret));
-            const authorization = `HMAC ${this.api_key}:${signature}`;
+            const Authorization = `HMAC ${this.api_key}:${signature}`;
             return {
-                date,
-                authorization,
+                headers: {
+                    Date: date,
+                    Authorization,
+                    'Content-Type': contentType
+                }
             };
-        };
-        this.test = () => {
-            console.log('hello world');
-            console.log(this.api_key);
-            console.log(this.api_secret);
-            console.log(this.callback_secret);
         };
     }
 }
-const tmp = new CryptoPay('api_secret', 'api_key', 'callback_secret');
-tmp.test();
+const testObj = new CryptoPay('api_secret', 'api_key', 'callback_secret');
+testObj.getRetes();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,46 @@
+import axios from "axios";
 import base64 from "crypto-js/enc-base64";
 import sha1 from "crypto-js/sha1";
 import md5 from "crypto-js/md5";
 
 class CryptoPay {
-  constructor(private api_secret: any,private api_key: any, private callback_secret:any) {}
+  constructor(
+    private api_secret: any,
+    private api_key: any,
+    private callback_secret: any
+  ) {}
 
-  private headerCreator = (method: string, path: string, body: any) => {
+  //Rates
+  public getRetes = async () => {
+    try {
+      const path = "/api/rates";
+      const headers = this.headerCreator("GET", path);
+      const response = await axios.get(path, headers);
+      console.log({ response });
+      return response;
+    } catch (error) {
+      throw new Error("getRetes: " + error);
+    }
+  };
+
+
+  private headerCreator = (method: string, path: string, body?: any) => {
     const date = new Date().toUTCString();
     const contentType = "application/json";
     const bodyHash = body ? md5(body).toString() : "";
 
     const stringToSign = `${method}\n${bodyHash}\n${contentType}\n${date}\n${path}`;
     const signature = base64.stringify(sha1(stringToSign, this.api_secret));
-    const authorization = `HMAC ${this.api_key}:${signature}`;
+    const Authorization = `HMAC ${this.api_key}:${signature}`;
     return {
-      date,
-      authorization,
+      headers: {
+        Date: date,
+        Authorization,
+        "Content-Type": contentType,
+      },
     };
   };
 }
 
-
-const testObj = new CryptoPay('api_secret', 'api_key', 'callback_secret')
+const testObj = new CryptoPay("api_secret", "api_key", "callback_secret");
+testObj.getRetes();

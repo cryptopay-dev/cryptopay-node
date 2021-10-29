495556ab489e7b8e050fa18e59d1cdc8700159db
Muchnik Illia <muchnik.illia.js@gmail.com>
2021-10-29 09:31:59 +0300

    Class CryptoPay in progress

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,28 @@ import base64 from "crypto-js/enc-base64";
 import sha1 from "crypto-js/sha1";
 import md5 from "crypto-js/md5";
 
+class CryptoPay {
+  private api_key;
+  private api_secret;
+  private callback_secret;
 
-
-const headerCreator = (method:string, path:string, api_secret:any, api_key:any, body:any) => {
-  const date = new Date().toUTCString();
-  const contentType = 'application/json';
-  const bodyHash = body ? md5(body).toString() : '';
-
-  const stringToSign = `${method}\n${bodyHash}\n${contentType}\n${date}\n${path}`;
-  const signature = base64.stringify(sha1(stringToSign, api_secret));
-  const authorization = `HMAC ${api_key}:${signature}`
-  return {
-    date,
-    authorization,
+  constructor(api_secret: any, api_key: any, callback_secret:any) {
+    this.api_key = api_key;
+    this.api_secret = api_secret;
+    this.callback_secret = callback_secret;
   }
-};
+
+  private headerCreator = (method: string, path: string, body: any) => {
+    const date = new Date().toUTCString();
+    const contentType = "application/json";
+    const bodyHash = body ? md5(body).toString() : "";
+
+    const stringToSign = `${method}\n${bodyHash}\n${contentType}\n${date}\n${path}`;
+    const signature = base64.stringify(sha1(stringToSign, this.api_secret));
+    const authorization = `HMAC ${this.api_key}:${signature}`;
+    return {
+      date,
+      authorization,
+    };
+  };
+}

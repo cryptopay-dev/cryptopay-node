b6084d86ec4e03d08a54cffa9d4434a3bb6dbe0d
Muchnik Illia <muchnik.illia.js@gmail.com>
2021-11-08 10:31:41 +0200

    Fix invoice params interface

--- src/interfaces/IInvoiceParams.ts
+++ src/interfaces/IInvoiceParams.ts
@@ -8,11 +8,11 @@ export interface IInvoiceParams {
   price_amount: string;
   price_currency: string;
   pay_currency: string;
-  custom_id: string;
-  customer_id: string;
-  name: string;
-  description: string;
-  metadata: any;
-  success_redirect_url: string;
-  unsuccess_redirect_url: string;
+  custom_id?: string;
+  customer_id?: string;
+  name?: string;
+  description?: string;
+  metadata?: any;
+  success_redirect_url?: string;
+  unsuccess_redirect_url?: string;
 }

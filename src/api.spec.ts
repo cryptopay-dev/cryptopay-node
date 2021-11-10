80a798db54abbc9c9f0087419b256d327ebe5faf
Muchnik Illia <muchnik.illia.js@gmail.com>
2021-11-10 14:06:01 +0200

    Add autotesting in progress

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -1,77 +1,99 @@
 import CryptoPay from "./app";
 import { IRate } from "./interfaces/IRate";
-import { MockProxy } from 'jest-mock-extended';
+import { MockProxy } from "jest-mock-extended";
 import { invoiceParamsToTest } from "./dataToTesting/invoiceParamsToTest";
-
-// const  CryptoPay  =require( "../src/app");
+import { ICustomError } from "./interfaces/ICustomError";
 const callback_secret = "sn8MGpjYipbVMv0oiU8FAYNRMkbAL9BZcYYSY28cnTE";
-// const api_key = "7AA2P-w0RxZXG-_K4cRngQ";
-// const api_secret = "NGR0vvNXKO_p3v2zz5ZuShP36Vp19ekZ9nLORtVZYpc";
 const api_key = "D-d6gn9axIWNPn5cPIukoA";
 const api_secret = "waNXkbUH7d-yRcImNM8vx9gLDX9ZgjTCpvtwX_anRyg";
 const cryptoPay = new CryptoPay(api_secret, api_key, callback_secret);
 // // var assert = require('assert');
-let invoiceID = ''
-const custom_id = 'PAYMENT-123'  // need to create dynamic
-let address =''
-let recalculation_id = ''
+let invoiceID = "";
+const custom_id = "PAYMENT-123"; // need to create dynamic
+let address = "";
+let recalculation_id = "";
 describe("Rates", () => {
-
   it("Get Retes has response", async () => {
     const resp = await cryptoPay.getRetes();
-    expect(resp).toBeTruthy()  
+    expect(resp).toBeTruthy();
   });
   it("Get Retes By Pair has response", async () => {
     const resp = await cryptoPay.getRetesByPair("BTC/EUR");
-    expect(resp).toBeTruthy()  
+    expect(resp).toBeTruthy();
   });
   it("Get Retes By Pair with wrong params", async () => {
     try {
       await cryptoPay.getRetesByPair("wrongParams");
     } catch (error) {
-      expect(error).toBeTruthy()
+      expect(error).toBeTruthy();
     }
   });
 });
 
-
 describe("Invoice", () => {
- it("Create invoice has response", async () => {
+  it("Create invoice has response", async () => {
     const resp = await cryptoPay.createInvoice(invoiceParamsToTest);
-    console.log({resp})
-    invoiceID = resp.id
-    address= resp.address
-    expect(resp).toBeTruthy()  
+    console.log({ resp });
+    invoiceID = resp.id;
+    address = resp.address;
+    expect(resp).toBeTruthy();
   });
+
   it("Get list invoice has response ", async () => {
     const resp = await cryptoPay.getListInvoces();
-    expect(resp).toBeTruthy()  
+    expect(resp).toBeTruthy();
   });
   it("Get invoice by invoice id has response ", async () => {
     const resp = await cryptoPay.getInvoceByInvoiceId(invoiceID);
-    expect(resp).toBeTruthy()  
-  });
-  it("Get list invoce by custom id has response ", async () => {
-    const resp = await cryptoPay.getListInvoceByCustomId(custom_id);
-    expect(resp).toBeTruthy()  
+    expect(resp).toBeTruthy();
   });
 
+  it("Get invoice by invoice id with wrong param ", async () => {
+    try {
+      await cryptoPay.getInvoceByInvoiceId("wrong params");
+    } catch (error) {
+      expect(error).toBeTruthy();
+    }
+  });
+
+  it("Get list invoce by custom id has response ", async () => {
+    const resp = await cryptoPay.getListInvoceByCustomId(custom_id);
+    expect(resp).toBeTruthy();
+  });
+
+  it("Get list invoce by custom id  with wrong param ", async () => {
+    try {
+      await cryptoPay.getListInvoceByCustomId("wrong params");
+    } catch (error) {
+      expect(error).toBeTruthy();
+    }
+  });
+
+
+  
   // Temporarily not working
   // it("Create recalculate invoices has response", async () => {
   //   const resp = await cryptoPay.createRecalculateInvoices(invoiceID, true);
-  //   expect(resp).toBeTruthy()  
+  //   expect(resp).toBeTruthy()
   // });
   // it("Create refund invoices has response", async () => {
   //   const resp = await cryptoPay.createInvoiceRefund(invoiceID ,address);
-  //   expect(resp).toBeTruthy()  
+  //   expect(resp).toBeTruthy()
   // });
   // it("Commit recalculate invoices has response", async () => {
   //   const resp = await cryptoPay.commitRecalculateInvoicesByIds(invoiceID ,recalculation_id);
-  //   expect(resp).toBeTruthy()  
+  //   expect(resp).toBeTruthy()
   // });
 
   it("Get list refund list has response", async () => {
-    const resp = await cryptoPay.getListInvoiceRefund(invoiceID );
-    expect(resp).toBeTruthy()  
+    const resp = await cryptoPay.getListInvoiceRefund(invoiceID);
+    expect(resp).toBeTruthy();
   });
-})
+  it("Get list refund list with wrong param ", async () => {
+    try {
+      await cryptoPay.getListInvoiceRefund("wrong params");
+    } catch (error) {
+      expect(error).toBeTruthy();
+    }
+  });
+});

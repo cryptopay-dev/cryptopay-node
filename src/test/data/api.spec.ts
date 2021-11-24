aab9d6ece701fd2954a832c2fa99a0d87526f868
Muchnik Illia <muchnik.illia.js@gmail.com>
2021-11-24 16:08:56 +0200

    Test in progress

--- src/test/data/api.spec.ts
+++ src/test/data/api.spec.ts
@@ -1,12 +1,13 @@
 import CryptoPay from "../../app";
-require('dotenv').config();
-import axios from 'axios'
+require("dotenv").config();
+import axios from "axios";
 import { invoiceParamsToTest } from "../../dataToTesting/invoiceParamsToTest";
 // responce to test
-import ratesPair from "./rates/pair.json"; 
-import list from "./rates/list.json"
-import errorToTest from "./error.json"
-import invoiceCreated from './invoice/invoice_created.json'
+import ratesPair from "./rates/pair.json";
+import list from "./rates/list.json";
+import errorToTest from "./error.json";
+import invoiceCreated from "./invoice/invoice_created.json";
+import invoiceList from "./invoice/invoices_list.json";
 
 const callback_secret = process.env.CALLBACK_SECRET || "";
 const api_key = process.env.API_KEY || "";
@@ -17,8 +18,7 @@ const custom_id = Date.now().toString();
 let address = "";
 let recalculation_id = "";
 
-
-jest.mock('axios')
+jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 //+
 // describe("Rates", () => {
@@ -61,165 +61,83 @@ describe("Invoice", () => {
       mockedAxios.request.mockResolvedValue(invoiceCreated);
       const resp = await cryptoPay
         .invoicesApi()
-        .invoicesCreate({ ...invoiceParamsToTest, custom_id });
+        .invoicesCreate({ ...invoiceParamsToTest });
       invoiceID = resp.data.id;
       address = resp.data.address;
       expect(resp).toEqual(invoiceCreated);
-      expect(mockedAxios.request).toHaveBeenCalledWith( {"headers": {}, "method": "POST", "url": `${cryptoPay.getUrl()}/api/invoices`});
-
-      // expect(resp).toBeTruthy();
-      // expect(typeof resp.data.id).toBe("string");
-      // expect((typeof resp.data.custom_id).toString()).toMatch(/string|object/);
-      // expect((typeof resp.data.customer_id).toString()).toMatch(
-      //   /string|object/
-      // );
-      // expect(typeof resp.data.status).toBe("string");
-      // expect((typeof resp.data.status_context).toString()).toMatch(
-      //   /string|object/
-      // );
-      // expect(typeof resp.data.address).toBe("string");
-      // expect(typeof resp.data.uri).toBe("string");
-      // expect(typeof resp.data.price_amount).toBe("string");
-      // expect(typeof resp.data.price_currency).toBe("string");
-      // expect(typeof resp.data.fee).toBe("string");
-      // expect(typeof resp.data.fee_currency).toBe("string");
-      // expect(typeof resp.data.pay_amount).toBe("string");
-      // expect(typeof resp.data.pay_currency).toBe("string");
-      // expect(typeof resp.data.paid_amount).toBe("string");
-      // expect(typeof resp.data.exchange).toBe("object");
-      // expect(typeof resp.data.transactions).toBe("object");
-      // expect((typeof resp.data.name).toString()).toMatch(/string|object/);
-      // expect((typeof resp.data.description).toString()).toMatch(
-      //   /string|object/
-      // );
-      // expect(typeof resp.data.metadata).toBe("object");
-      // expect((typeof resp.data.success_redirect_url).toString()).toMatch(
-      //   /string|object/
-      // );
-      // expect((typeof resp.data.unsuccess_redirect_url).toString()).toMatch(
-      //   /string|object/
-      // );
-      // expect(typeof resp.data.hosted_page_url).toBe("string");
-      // expect(typeof resp.data.created_at).toBe("string");
-      // expect(typeof resp.data.expires_at).toBe("string");
+      expect(mockedAxios.request).toHaveBeenCalledWith({
+        data: '{"price_amount":100,"price_currency":"EUR","pay_currency":"BTC"}',
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+        url: "https://business-sandbox.cryptopay.me/api/invoices",
+      });
+    } catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
+  //invoiceList
+  it("Get list invoice has response ", async () => {
+    try {
+      mockedAxios.request.mockResolvedValue(invoiceList);
+      const resp = await cryptoPay.invoicesApi().invoicesList();
+      expect(resp).toEqual(invoiceList);
+      expect(mockedAxios.request).toHaveBeenCalledWith({
+        headers: {},
+        method: "GET",
+        url: "https://business-sandbox.cryptopay.me/api/invoices",
+      });
     } catch (error) {
       expect(error).toBeFalsy();
     }
   });
 
-  // it("Get list invoice has response ", async () => {
-  //   try {
-  //     // axiosVCR.mountCassette('./test/fixtures/invoicesList.json')
-  //     const resp = await cryptoPay.invoicesApi().invoicesList();
-  //     // axiosVCR.ejectCassette('./test/fixtures/invoicesList.json')
-  //     expect(typeof resp.data).toBe("object");
-  //     expect(typeof resp.meta).toBe("object");
-  //   } catch (error) {
-  //     expect(error).toBeFalsy();
-  //   }
-  // });
+  it("Get invoice by invoice id validate response ", async () => {
+    try {
+      mockedAxios.request.mockResolvedValue(invoiceCreated);
+      const resp = await cryptoPay.invoicesApi().invoicesRetrieve(invoiceID);
+      expect(resp).toEqual(invoiceCreated);
+      expect(mockedAxios.request).toHaveBeenCalledWith({
+        headers: {},
+        method: "GET",
+        url: `${cryptoPay.getUrl()}/api/invoices/${invoiceID}`,
+      });
+    } catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
 
-  // it("Get invoice by invoice id validate response ", async () => {
-  //   try {
-  //     // axiosVCR.mountCassette('./test/fixtures/invoicesListByInvoiceId.json')
-  //     const resp = await cryptoPay.invoicesApi().invoicesRetrieve(invoiceID);
-  //     // axiosVCR.ejectCassette('./test/fixtures/invoicesListByInvoiceId.json')
+  it("Get invoice by invoice id with wrong param ", async () => {
+    try {
+      mockedAxios.request.mockRejectedValue(errorToTest);
+      await cryptoPay.invoicesApi().invoicesList("wrongParams");
+      expect(mockedAxios.request).toHaveBeenCalledWith({
+        headers: {},
+        method: "GET",
+        url: `${cryptoPay.getUrl()}/api/invoices/wrongParams`,
+      });
+    } catch (error) {
+      expect(error).toBeTruthy();
+      expect(error).toEqual(errorToTest);
+    }
+  });
 
-  //     expect(typeof resp.data.id).toBe("string");
-  //     expect((typeof resp.data.custom_id).toString()).toMatch(/string|object/);
-  //     expect((typeof resp.data.customer_id).toString()).toMatch(
-  //       /string|object/
-  //     );
-  //     expect(typeof resp.data.status).toBe("string");
-  //     expect((typeof resp.data.status_context).toString()).toMatch(
-  //       /string|object/
-  //     );
-  //     expect(typeof resp.data.address).toBe("string");
-  //     expect(typeof resp.data.uri).toBe("string");
-  //     expect(typeof resp.data.price_amount).toBe("string");
-  //     expect(typeof resp.data.price_currency).toBe("string");
-  //     expect(typeof resp.data.fee).toBe("string");
-  //     expect(typeof resp.data.fee_currency).toBe("string");
-  //     expect(typeof resp.data.pay_amount).toBe("string");
-  //     expect(typeof resp.data.pay_currency).toBe("string");
-  //     expect(typeof resp.data.paid_amount).toBe("string");
-  //     expect(typeof resp.data.exchange).toBe("object");
-  //     expect(typeof resp.data.transactions).toBe("object");
-  //     expect((typeof resp.data.name).toString()).toMatch(/string|object/);
-  //     expect((typeof resp.data.description).toString()).toMatch(
-  //       /string|object/
-  //     );
-  //     expect(typeof resp.data.metadata).toBe("object");
-  //     expect((typeof resp.data.success_redirect_url).toString()).toMatch(
-  //       /string|object/
-  //     );
-  //     expect((typeof resp.data.unsuccess_redirect_url).toString()).toMatch(
-  //       /string|object/
-  //     );
-  //     expect(typeof resp.data.hosted_page_url).toBe("string");
-  //     expect(typeof resp.data.created_at).toBe("string");
-  //     expect(typeof resp.data.expires_at).toBe("string");
-  //   } catch (error) {
-  //     expect(error).toBeFalsy();
-  //   }
-  // });
-
-  // it("Get invoice by invoice id with wrong param ", async () => {
-  //   try {
-  //     // axiosVCR.mountCassette('./test/fixtures/invoicesListByInvoiceIdWithWrongParams.json')
-  //     await cryptoPay.invoicesApi().invoicesList("wrong params");
-  //     // axiosVCR.ejectCassette('./test/fixtures/invoicesListByInvoiceIdWithWrongParams.json')
-  //   } catch (error) {
-  //     expect(error).toBeTruthy();
-  //   }
-  // });
-
-  // it("Get list invoce by custom id validate response ", async () => {
-  //   try {
-  //     // axiosVCR.mountCassette('./test/fixtures/invoicesRetrieveByCustomId.json')
-  //     const resp = await cryptoPay
-  //       .invoicesApi()
-  //       .invoicesRetrieveByCustomId(custom_id);
-  //     // axiosVCR.ejectCassette('./test/fixtures/invoicesRetrieveByCustomId.json')
-  //     expect(resp).toBeTruthy();
-  //     expect(typeof resp.data.id).toBe("string");
-  //     expect((typeof resp.data.custom_id).toString()).toMatch(/string|object/);
-  //     expect((typeof resp.data.customer_id).toString()).toMatch(
-  //       /string|object/
-  //     );
-  //     expect(typeof resp.data.status).toBe("string");
-  //     expect((typeof resp.data.status_context).toString()).toMatch(
-  //       /string|object/
-  //     );
-  //     expect(typeof resp.data.address).toBe("string");
-  //     expect(typeof resp.data.uri).toBe("string");
-  //     expect(typeof resp.data.price_amount).toBe("string");
-  //     expect(typeof resp.data.price_currency).toBe("string");
-  //     expect(typeof resp.data.fee).toBe("string");
-  //     expect(typeof resp.data.fee_currency).toBe("string");
-  //     expect(typeof resp.data.pay_amount).toBe("string");
-  //     expect(typeof resp.data.pay_currency).toBe("string");
-  //     expect(typeof resp.data.paid_amount).toBe("string");
-  //     expect(typeof resp.data.exchange).toBe("object");
-  //     expect(typeof resp.data.transactions).toBe("object");
-  //     expect((typeof resp.data.name).toString()).toMatch(/string|object/);
-  //     expect((typeof resp.data.description).toString()).toMatch(
-  //       /string|object/
-  //     );
-  //     expect(typeof resp.data.metadata).toBe("object");
-  //     expect((typeof resp.data.success_redirect_url).toString()).toMatch(
-  //       /string|object/
-  //     );
-  //     expect((typeof resp.data.unsuccess_redirect_url).toString()).toMatch(
-  //       /string|object/
-  //     );
-  //     expect(typeof resp.data.hosted_page_url).toBe("string");
-  //     expect(typeof resp.data.created_at).toBe("string");
-  //     expect(typeof resp.data.expires_at).toBe("string");
-  //   } catch (error) {
-  //     expect(error).toBeFalsy();
-  //   }
-  // });
+  it("Get list invoce by custom id validate response ", async () => {
+    try {
+      mockedAxios.request.mockResolvedValue(invoiceCreated);
+      const resp = await cryptoPay
+        .invoicesApi()
+        .invoicesRetrieveByCustomId(custom_id);
+      expect(resp).toBeTruthy();
+      expect(resp).toEqual(invoiceCreated);
+      expect(mockedAxios.request).toHaveBeenCalledWith({
+        headers: {},
+        method: "GET",
+        url: `${cryptoPay.getUrl()}/api/invoices/custom_id/${custom_id}`,
+      });
+    } catch (error) {
+      expect(error).toBeFalsy();
+    }
+  });
 
   // it("Get list invoce by custom id  with wrong param ", async () => {
   //   try {

2037d2a82991b347c4db04fd9f968fac02bca3c7
Muchnik Illia <muchnik.illia.js@gmail.com>
2021-12-06 10:24:24 +0200

    Fix type

--- scriptToGenerateCode.sh
+++ scriptToGenerateCode.sh
@@ -1,1 +1,1 @@
-openapi-generator-cli generate -i openapi.yml -g typescript-axios -o openApiGeneretedCode --additional-properties=supportsES6=true  --type-mappings Decimal=string
+openapi-generator-cli generate -i openapi.yml -g typescript-axios -o openApiGeneretedCode --additional-properties=supportsES6=true  --type-mappings Decimal=number

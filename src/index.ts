61324300e42ee922bf1b844cfe48d19041536aeb
tulenkovR <kubmaster@mail.ru>
2021-12-08 15:58:28 +0300

    Refactor coding

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,34 @@ dotenv.config();
 export default class CryptoPay {
   /* eslint-disable @typescript-eslint/no-explicit-any*/
   private InvoicesApi: any;
-  /* eslint-disable @typescript-eslint/no-explicit-any*/
   private RatesApi: any;
+  /* eslint-enable @typescript-eslint/no-explicit-any*/
+
   public readonly url: string = SERVER.sandbox;
+
   constructor(private apiSecret: string, private apiKey: string, private callbackSecret: string, url?: string) {
     if (url && Object.values(SERVER).includes(url)) {
       this.url = url;
-    } else {
-      this.url = SERVER.sandbox;
     }
+
     const customizedAxios = this.customizationAxios();
     this.InvoicesApi = openApiGeneretedCode.InvoicesApiFactory(undefined, this.url, customizedAxios);
     this.RatesApi = openApiGeneretedCode.RatesApiFactory(undefined, this.url, customizedAxios);
   }
+
+  public invoicesApi = () => {
+    return this.InvoicesApi;
+  };
+
+  public ratesApi = () => {
+    return this.RatesApi;
+  };
+
+  /* eslint-disable @typescript-eslint/no-explicit-any*/
+  public callbackVerification = (body: string, headers: any): boolean => {
+    return CryptoJS.HmacSHA256(body, this.callbackSecret).toString() === headers['x-cryptopay-signature'];
+  };
+  /* eslint-enable @typescript-eslint/no-explicit-any*/
 
   private customizationAxios = () => {
     axios.interceptors.request.use((req) => {
@@ -45,19 +60,7 @@ export default class CryptoPay {
     return axios;
   };
 
-  // public getUrl = () => {
-  //   return this.url;
-  // };
-  public invoicesApi = () => {
-    return this.InvoicesApi;
-  };
-  public ratesApi = () => {
-    return this.RatesApi;
-  };
   /* eslint-disable @typescript-eslint/no-explicit-any*/
-  public callbackVerification = (body: string, headers: any): boolean => {
-    return CryptoJS.HmacSHA256(body, this.callbackSecret).toString() === headers['x-cryptopay-signature'];
-  };
   private headerCreator(method: string, path: string, body?: any): IHeaders {
     const date = new Date(Date.now()).toUTCString();
     const contentType = 'application/json';
@@ -73,4 +76,5 @@ export default class CryptoPay {
       },
     };
   }
+  /* eslint-enable @typescript-eslint/no-explicit-any*/
 }
